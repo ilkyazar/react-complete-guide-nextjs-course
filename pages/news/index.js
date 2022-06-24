0ff0d59315eb4058f95d692c16ff07c97379bcf8
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-24 19:54:07 +0300

    linking pages with next/Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
