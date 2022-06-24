48d9769c809376d1a21b7a16d8ab495973d723eb
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-24 19:50:46 +0300

    extract dynamic param newsId

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router';
+
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to the backend with newsId
+
   return <h1>The Detail Page</h1>;
 }
 
